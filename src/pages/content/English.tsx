6257736a7de392f785c6ffa2657e09e07f9c7da9
LegendaryHori <shotakobayashi7@gmail.com>
2025-05-21 17:06:19 +0900

    commit

--- src/pages/content/English.tsx
+++ src/pages/content/English.tsx
@@ -4,6 +4,7 @@ import Buttons from "@/components/ui/button_ui";
 
 export default function ProtectedPage() {
     const { loading } = useAuth();
+    const { user } = useAuth();
 
     if (loading) return <p>Loding...</p>;
 
@@ -22,17 +23,17 @@ export default function ProtectedPage() {
                     </h1>
                     <div className="m-auto">
                         <p className=" p-[3rem]">
-                            ankiを使用し、英単語暗記を手伝います。
-                        </p>
-                        <p><span className="font-bold text-[20px]">ankiとは？</span><br />
+                            ankiを使用し、英単語暗記を手伝います。<br /><br />
+                            <span className="font-bold text-[20px]">ankiとは？</span><br />
                             単語帳のソフトウェア版です。<br />
                             忘却曲線という人がどれくらい早く忘れてしまい、どのタイミングで復習すれば良いのかという研究結果を参考にして作成されています。<br />
                             これによって最大効率で学びが記憶に残るような学習を提供しています。<br />
-                            我々はこのanki内の単語帳を学校の教材を基に作成し、提供していきたいと思います。</p>
+                            我々はこのanki内の単語帳を学校の教材を基に作成し、提供していきたいと思います。
+                        </p>
                     </div>
                 </div>
                 <div className=" w-full text-center mt-[0.5rem]">
-                    <Buttons className=" w-[12rem] h-[3rem]" onClick={Onclick} text="購入はこちら" />
+                    {user ? <Buttons className=" w-[12rem] h-[3rem]" onClick={Onclick} text="購入はこちら" /> : ""}
                 </div>
                 {/*<a className=" m-auto" href="/anku/001.apkg" download>aaaaa</a>*/}
             </div>
